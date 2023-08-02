6df7305f77d3621043b5bf5fb6a0acfa98d38af1
Sumon <sumonkhan1081@gmail.com>
2023-08-03 03:22:28 +0530

    page5 completed

--- script.js
+++ script.js
@@ -7,19 +7,19 @@ function loder() {
     let tl = gsap.timeline();
 
     tl
-        .to(".main", {
+        .to(".page1", {
             y: "100vh",
-            scale: 0.4,
+            scale: 0.5,
             duration: 0,
         })
-        .to(".main", {
-            y: "-60vh",
+        .to(".page1", {
+            y: "10vh",
             duration: 1,
             delay: 0.5,
         })
-        .to(".main", {
-            y: 0,
-            rotate: 360,
+        .to(".page1", {
+            y: "0vh",
+            rotate: -360,
             scale: 1,
             duration: 0.6,
         })
